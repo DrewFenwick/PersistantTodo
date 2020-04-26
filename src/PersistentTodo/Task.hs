ce2f56f964249ec34a93e744e4cf61e19d3fc8d8
DrewFenwick <drew.e.fenwick+github@gmail.com>
2020-04-26 12:03:45 +0100

    Create taskTable and taskSelect with Opaleye
    Also addded product-profunctors to dependencies.

--- src/PersistentTodo/Task.hs
+++ src/PersistentTodo/Task.hs
@@ -1,19 +1,41 @@
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+
 module PersistentTodo.Task
   ( Status(..)
-  , Task(..)
+  , Task'(..)
+  , Task
   , setStatus
   )
 where
+
+import           Opaleye
+import           Data.Profunctor.Product        ( p2
+                                                , p3
+                                                )
+import           Data.Profunctor.Product.TH     ( makeAdaptorAndInstance )
 
 data Status
   = Pending
   | Completed
 
-data Task = Task
-  { title :: String
-  , status :: Status
+data Task' t s = Task
+  { title :: t
+  , status :: s
   }
+
+type Task = Task' String Status
+type TaskField = Task' (Field SqlText) (Field SqlBool)
+$(makeAdaptorAndInstance "pTask" ''Task')
 
 setStatus :: Status -> Task -> Task
 setStatus status task = task { status = status }
 
+taskTable :: Table TaskField TaskField
+taskTable = table
+  "taskTable"
+  (pTask Task { title = tableField "title", status = tableField "status" })
+
+taskSelect :: Select TaskField
+taskSelect = selectTable taskTable
