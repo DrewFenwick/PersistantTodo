a5a8898b34d3e257371a2e35917e096847316533
DrewFenwick <drew.e.fenwick+github@gmail.com>
2020-04-26 22:48:02 +0100

    Finish handle function

--- src/PersistentTodo/Command/Handler.hs
+++ src/PersistentTodo/Command/Handler.hs
@@ -7,7 +7,9 @@ module PersistentTodo.Command.Handler
   )
 where
 
-import           PersistentTodo.Command         ( Command(..) )
+import           PersistentTodo.Command         ( Command(..)
+                                                , Place
+                                                )
 import           Control.Monad.Reader.Class
 import           Control.Monad.IO.Class
 import           Database.PostgreSQL.Simple     ( Connection )
@@ -16,9 +18,27 @@ type HandlerStack m = (MonadIO m, MonadReader m, EnvType m ~ Connection)
 
 handle :: HandlerStack m => Command -> m ()
 handle = \case
-  Add      place -> undefined
-  Complete place -> undefined
-  Move start end -> undefined
-  Remove place   -> undefined
-  Clean          -> undefined
-  Wipe           -> undefined
+  Add      title -> add title
+  Complete place -> complete place
+  Move start end -> move start end
+  Remove place   -> remove place
+  Clean          -> clean
+  Wipe           -> wipe
+
+add :: HandlerStack m => String -> m ()
+add = undefined
+
+complete :: HandlerStack m => Place -> m ()
+complete = undefined
+
+move :: HandlerStack m => Place -> Place -> m ()
+move = undefined
+
+remove :: HandlerStack m => Place -> m ()
+remove = undefined
+
+clean :: HandlerStack m => m ()
+clean = undefined
+
+wipe :: HandlerStack m => m ()
+wipe = undefined
