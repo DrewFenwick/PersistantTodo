29c3ff6575d9cb6f49c0b42bd847a53dedd555ca
DrewFenwick <drew.e.fenwick+github@gmail.com>
2020-04-26 22:14:55 +0100

    Convert the return type of handle to a transformer stack

--- src/PersistentTodo/Command/Handler.hs
+++ src/PersistentTodo/Command/Handler.hs
@@ -1,9 +1,17 @@
+{-# LANGUAGE TypeFamilies #-}
+
 module PersistentTodo.Command.Handler
   ( handle
   )
 where
 
 import           PersistentTodo.Command         ( Command )
+import           Control.Monad.Reader.Class
+import           Control.Monad.IO.Class
+import           Database.PostgreSQL.Simple     ( Connection )
 
-handle :: Command -> IO ()
+handle
+  :: (MonadIO m, MonadReader m, EnvType m ~ Connection)
+  => Command
+  -> m Connection
 handle = undefined
